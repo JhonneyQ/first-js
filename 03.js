72ab5e217a9ef63041e24b280a8c9f5c1172d4f2
jhonnyq <kananqadirov2005@gmail.com>
2024-10-30 19:29:16 +0400

    commit

---
+++ 03.js
@@ -0,0 +1,153 @@
+// for (let index = 0; index <= 100; index++) {
+//     console.log(index);
+    
+// }
+
+
+// cf
+
+// let sum = 0
+// for (let index = 0; index <= 100; index++) {
+//     if(index%5==0){
+//         sum +=index
+//     }
+    
+// }
+// console.log(sum)
+
+
+
+
+
+// let arr =[12,14,35,56,68,93];
+
+
+
+
+// let sum = 0;
+// for (let index = 0; index < arr.length; index++) {
+//     sum += arr[index]
+    
+// }
+// console.log(sum)
+
+
+// for (let index = 0; index < arr.length; index++) {
+//     if(index%2== 1){
+//         console.log(arr[index])
+//     }
+    
+// }
+
+// for (let index = 0; index < arr.length; index++) {
+//     if(index%2== 0){
+//         console.log(arr[index])
+//     }
+    
+// }
+
+// for (let index = 0; index < arr.length; index++) {
+//     console.log(arr[arr.length-1])
+    
+// }
+
+// for (let index = 0; index < arr.length; index++) {
+//     console.log(arr[arr.length-1]-arr[0])
+    
+// }
+
+
+// let a = +prompt("eded daxil et")
+// for (let index = 0; index < arr.length; index++) {
+//     if(a === arr[index]){
+//         console.log("bu eded var");
+//         break
+
+//     }
+    
+// }
+
+// const employees = [
+//     { name: "Jamil", salary: 50000, department: "IT" },
+//     { name: "Jale", salary: 60000, department: "HR" },
+//     { name: "Bayram", salary: 55000, department: "IT" },
+//     { name: "Sahil", salary: 75000, department: "HR" },
+//     { name: "Maryam", salary: 65000, department: "IT" },
+//     { name: "Elnara", salary: 80000, department: "HR" },
+//     { name: "Davud", salary: 70000, department: "IT" },
+//   ];
+
+// for (let index = 0; index < employees.length; index++) {
+//     if(employees[index].salary>60000){
+//         console.log(employees[index])
+//     }
+    
+// }
+
+// let b = 1
+// let a = prompt("eded daxil edin");
+// for (let index = 1; index <= a; index++) {
+//     b = b*index
+    
+// }
+
+// console.log(b)
+
+
+// let a = "lorem ipsum dolor"
+// count = 0
+// for (let index = 0; index < a.length; index++) {
+//     if(a[index]==="o"){
+//         count++
+//     }
+    
+// }
+// console.log(count)
+
+
+// let a = ["azerbaijan", "america", "albania" ]
+
+// for (let index = 0; index < a.length; index++) {
+//     if(a[index][0]==="a" && a[index][a[index].length-1] == "a"){
+//         console.log(a[index])
+//     }
+    
+// }
+// let a = 1;
+// let n = 437;
+// console.log(n[0])
+// while(a === 3){
+//     console.log(n[a]);
+//     a += 1;
+// }
+
+
+// let a = +prompt("eded");
+// for (let index = 2; index <= 5; index++) {
+//     if(a == 2 || a==3 || a == 5 ){
+//         console.log("sadedir")
+//         break
+        
+//     }else if( a%index===0){
+//         console.log("murekkeb")
+//     }else if(a%index!==0){
+//         console.log("sadedir")
+//     }
+    
+// }
+let a = 0
+const scores = [60, 75, 88, 93, 45, 100];
+let b = scores[0]
+for (let index = 0; index < scores.length; index++) {
+    a+=scores[index]
+    if(b<){
+        console.log(b);
+
+    }
+    
+    
+    
+    
+    
+}
+console.log(a/scores.length)
