e8b22023bc3259dd3decbbc5f511f0e33cb5e75e
jhonnyq <kananqadirov2005@gmail.com>
2024-10-31 18:51:20 +0400

    commit

--- 03.js
+++ 03.js
@@ -135,19 +135,180 @@
 //     }
     
 // }
-let a = 0
-const scores = [60, 75, 88, 93, 45, 100];
-let b = scores[0]
-for (let index = 0; index < scores.length; index++) {
-    a+=scores[index]
-    if(b<){
-        console.log(b);
+// let a = 0
+// const scores = [60, 75, 88, 93, 45, 100];
+// let b = scores[0]
+// for (let index = 0; index < scores.length; index++) {
+//     a+=scores[index]
+//     if(b<){
+//         console.log(b);
 
-    }
+//     }
     
     
     
     
     
-}
-console.log(a/scores.length)
+// }
+// console.log(a/scores.length)
+
+
+// let array = [12,23,43,54,12,65,67,89,90,100]
+// let a = array[0]
+// let b = array[0]
+
+// for (let index = 0; index < array.length; index++) {
+//     if(array[index]>a){
+//          a = array[index]
+        
+//     }
+//     if(array[index]<b){
+//          b = array[index]
+//     }
+// }
+// let c = array.pop()
+// array.unshift(a)
+// array.push(b)
+// console.log(array)
+
+// for (let index = 0; index < array.length; index++) {
+//     if(array[index]%2==0){
+//         if(array[index]>a){
+//             a = array[index]
+//         }
+//     }
+    
+// }
+// console.log(a)
+
+// for (let index = 0; index < array.length; index++) {
+//     if(array[index] %2==1){
+//         if(array[index]>a){
+//             a = array[index]
+//         }
+//     }
+    
+// }
+// console.log(a)
+
+// for (let index = 0; index < array.length; index++) {
+//     if(array[index]>a){
+//         a = array[index]
+        
+//     }
+//     if(array[index]<a){
+//         b = array[index]
+//     }
+// }
+// let hasil = a*b
+// let cem = a+b
+// console.log("hasil =",hasil , "cem = ", cem)
+// if(hasil > cem){
+//     console.log("hasil boyukdur")
+// }else{
+//     console.log("cem boyukdur")
+// }
+// let req1
+// let req2
+
+// for (let index = 0; index < array.length; index++) {
+//     let c =  String(array)
+//     req1 = +c[0]
+//     req2 = +c[1]
+    
+// }
+// console.log(req1+req2)
+
+
+
+// for (let index = 0; index < array.length; index++) {
+//     if(array[index]=a){
+//          console.log(array[index])
+        
+//     }
+    
+// }
+
+
+// let people = [
+//     { name: "Aysel", age: 25 },
+//     { name: "Rəşad", age: 30 },
+//     { name: "Leyla", age: 18 },
+//     { name: "Kamal", age: 40 }
+// ];
+// sum = 0
+// for (let index = 0; index < people.length; index++) {
+//     sum += people[index].age
+// }
+
+// console.log(sum)
+
+// let people = [
+//     { name: "Aysel", age: 25, city: "Bakı" },
+//     { name: "Rəşad", age: 30, city: "Gəncə" },
+//     { name: "Leyla", age: 18, city: "Bakı" },
+//     { name: "Kamal", age: 40, city: "Sumqayıt" }
+// ];
+
+// for (let index = 0; index < people.length; index++) {
+//     if(people[index].city="Bakı" && people[index].age>20)
+//     console.log(people[index])
+// }
+
+// let array1 = [1, 3, 5, 7];
+
+// let array2 = [2, 3, 6, 8, 7];
+// let ber 
+// for (let index = 0; index < array1.length; index++) {
+//     ber = array1[index]
+//     for (let index = 0; index < array2.length; index++) {
+//         if(ber==array2[index]){
+//             console.log(ber)
+//         }
+        
+//     }
+        
+    
+// }
+
+
+// let array =[-10,12,-23,40,60,73,-6,-20]
+// let countmin = 0
+// let countmax = 0
+// for (let index = 0; index < array.length; index++) {
+//     if(array[index]<0){
+//         countmin++
+//     }
+//     if(array[index]>0){
+//         countmax++
+//     }
+    
+// }
+
+// console.log(countmin)
+// console.log(countmax
+
+// )
+// let hasil = 1
+// let hasil2 = 1
+// for (let index = 0; index < array.length; index++) {
+//     if(array[index]<0){
+//         hasil *= array[index]
+//     }
+//     if(array[index]>0){
+//         hasil2 *= array[index]
+//     }
+    
+// }
+
+// console.log(hasil)
+// console.log(hasil2)
+    
+// let array =[11,12,14,56]
+
+// for (let index = array.length; index >= 0; index++) {
+//     console.log(array[index])
+    
+// }
+
+
