775981bb3f09620dfc3e9399b1a5076cb13edf5c
jhonnyq <kananqadirov2005@gmail.com>
2024-11-01 18:04:53 +0400

    commit

---
+++ 04.js
@@ -0,0 +1,169 @@
+// function fullname(lorem,  ipsum) {
+//     return lorem + ipsum;
+//   }
+
+// console.log(fullname("saalam","necesen"))
+
+
+//   function square(a){
+//         return a**2
+//   }
+//   a = prompt("eded")
+//   console.log(square(a))
+
+// function sum(a,b){
+//     return a+b
+// }
+
+// function minus(a,b){
+//     return a-b
+// }
+
+// function vurma(a,b){
+//     return a*b
+// }
+
+// function divide(a,b){
+//     return a/b 
+// }
+// c = prompt("operator elvae ele")
+// console.log(c(5,6))
+
+// function array(a){
+    
+//    return a[a.length-1]+a[0]
+// }
+
+// console.log(array([80, 5, 58, 17, 100]))
+
+// function sum1(a){
+//     sum = 0
+//     for (let index = 0; index < a.length; index++) {
+//         sum += a[index]
+        
+//     }
+//     return sum
+// }
+
+// console.log(sum1([80, 5, 58, 17, 100]))
+
+
+// ,function max(a){
+//     b = a[0]
+//     for (let index = 0; index < a.length; index++) {
+//         if(a[index]>b){
+//             b=a[index]
+//         }
+        
+//     }
+//     return b
+// }
+
+// console.log(max([80, 5, 58, 17, 100]))
+
+// function kv(a){
+//     for (let index = 0; index <= a; index++) {
+//          console.log(index**2)
+        
+//     }
+    
+// }
+
+// console.log(kv(5))
+
+// function bolme(a){
+//     if(a%30==0){
+//         console.log("bolunur")
+//     }
+// }
+
+// console.log(bolme(60))
+
+// function sum1(a){
+//     sum = 0
+//     for (let index = 0; index < a.length; index++) {
+//         sum += a[index]
+        
+//     }
+//     return sum
+// }
+
+// console.log(sum1([80, 5, 58, 17, 100]))
+
+// function ters(a){
+//     let b=[]
+//     for (let index = a.length-1; index >= 0; index--) {
+//         b.push(a[index])
+        
+//     }
+//     return b
+// }
+
+// console.log(ters([80, 5, 58, 17, 100]))
+
+// function qiy(a){
+//     if(a > 60){
+//         console.log("kecdin")
+//     }else{
+//         console.log("qaldin")
+//     }
+// }
+
+// console.log(qiy(61))
+
+
+// function yoxla(a){
+//     if(a == 10){
+//         console .log("eded beraberdi ")
+//     }else if(a<10){
+//         console.log("eded kicidkdir")
+//     }else{
+//         console.log("eded boyukdur")
+//     }
+// }
+
+// console.log(yoxla(9))
+
+// function max(a){
+//     let b = 0
+//     for (let index = 0; index < a.length; index++) {
+//         if(a[index] >b){
+//             b=a[index]
+//         }
+        
+//     }
+//     return b
+// }
+
+// console.log(max([80, 5, 58, 17, 100]))
+
+// function cut(a){
+//     let count = 0
+//     for (let index = 0; index < a.length; index++) {
+//         if(a[index]%2==0){
+//             count++
+//         }
+        
+        
+//     }
+//     return count
+// }
+
+// console.log(cut([80, 5, 58, 17, 100]))
+
+// function yox(a){
+//     for (let index = 0; index < a.length; index++) {
+//         if(a[index]=="a" || a[index]=="e" || a[index]=="i" || a[index]=="o" || a[index]=="u" ){
+            
+//             return console.log("var")
+//         }else{
+//             return console.log("yoxdur")
+//         }
+        
+//     }
+    
+// }
+
+// console.log(yox("mmm"))
+
+
