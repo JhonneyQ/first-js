14671f866b748c47f1d0baca4613d7b1115be146
jhonnyq <kananqadirov2005@gmail.com>
2024-11-06 19:22:28 +0400

    commit

---
+++ 08.js
@@ -0,0 +1,57 @@
+// class person{
+//     constructor(name,age,height){
+//         this.name=name
+//         this.age=age
+//         this.height=height
+//     }
+//     getsay(){
+//         return "adamlar"
+//     }
+// }
+
+
+// class person1 extends person{
+//     constructor(name,personality){
+//         super(name)
+//         this.personality=personality
+//     }
+//     getsay(){
+//         return "-"
+//     }
+//     getfullname(){
+//         return this.name + this.getsay + this.age
+//     }
+// }
+
+
+// let name1 = new person1("aslan")
+// console.log(name1.name)
+
+
+// class hesab{
+//     constructor(eded1){
+//         this.eded1 =eded1
+        
+
+//     }
+//     getbolme(num){
+//         this.eded1/=num
+//         return this
+//     }
+//     getvurma(num){
+//         this.eded1*=num
+//         return this
+//     }ws
+//     getustunegel(num){
+//         this.eded1+=num
+//         return this
+//     }
+//     getcix(num){
+//         this.eded1-=num
+//         return thisew
+//     }
+    
+// }
+
+// let a = new hesab(20).getvurma(2)
+// console.log(a)
