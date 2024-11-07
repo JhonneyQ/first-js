9ba912c624d513dc75fba5abbc3bf4b921024e36
jhonnyq <kananqadirov2005@gmail.com>
2024-11-07 18:59:18 +0400

    salam

---
+++ 09.js
@@ -0,0 +1,16 @@
+const div = document.createElement("div");
+const h2 = document.createElement("h2");
+
+div.append(h2);
+document.body.appendChild(div)
+
+
+
+div.style.width="200px"
+div.style.height="200px"
+div.style.border="2px solid red"
+div.style.borderRadius="17px"
+div.style.backgroundColor="green"
+h2.textContent="salam"
+h2.style.textAlign = "center"
+h2.style.color = "white"
